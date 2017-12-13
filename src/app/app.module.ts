7330d308e349cdbc3169a27a21b3536561270a69
S. Kyle Korndoerfer <kyle.korndoerfer@xerox.com>
2017-12-13 15:24:44 -0500

    Flushed out Welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppBootstrapModule } from './app-bootstrap.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
